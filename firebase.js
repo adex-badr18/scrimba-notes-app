394b24aade1c9017e04b247a010a63316c364d82
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-05 08:59:31 +0100

    Setup firebase

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { collection, getFirestore } from "firebase/firestore"; // import firestore service
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ9rcVBIAM0hXXkDKveeH5GDvWySgGZN4",
+  authDomain: "react-notes-adccb.firebaseapp.com",
+  projectId: "react-notes-adccb",
+  storageBucket: "react-notes-adccb.appspot.com",
+  messagingSenderId: "138457094629",
+  appId: "1:138457094629:web:fb746617a7e5225e58f70c"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); // returns an instance of the database
+const notesCollection = collection(db, 'notes');
